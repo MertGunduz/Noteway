a84c85c33248b37be30441ce3c1bebcae84c7ee7
Mert Gunduz <merttgg@gmail.com>
2022-04-17 15:12:04 +0300

    Source Added

---
+++ Forms/Noteway_AwakeForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Noteway.Forms
+{
+    public partial class Noteway_AwakeForm : Form
+    {
+        public Noteway_AwakeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
